a371f105a9797fa3c646b05730805aa4c2a244a4
virginia-m <virginia.m.frey@gmail.com>
2019-01-23 07:28:07 +1100

    More examples and problems for packaging

--- day-1-python-modules-and-git/examples/example_packages/setup.py
+++ day-1-python-modules-and-git/examples/example_packages/setup.py
@@ -7,7 +7,7 @@ setup(
     name='example_package',
     version='0.0',
     description='An example Python module',
-    author='Mr. X',
+    author='Mr. Neutron',
     author_email='python@equs.org',
     packages=['example_package']
 )
